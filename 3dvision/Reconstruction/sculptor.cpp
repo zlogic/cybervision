0e6db478f26113bf8dd7e254d7257282acad681b
Dmitry Zolotukhin <zlogic@gmail.com>
2010-02-05 23:53:21 +0300

    - Imported history 3dvision_2010-02-05 - Voronoi

--- 3dvision/Reconstruction/sculptor.cpp
+++ 3dvision/Reconstruction/sculptor.cpp
@@ -18,15 +18,54 @@ bool operator<(const QPoint& a,const QPoint& b){
 		return false;
 }
 
+bool operator<(const QPointF& a,const QPointF& b){
+	if(a.x()<b.x())
+		return true;
+	else if(qFuzzyCompare(a.x(),b.x()))
+		return a.y()<b.y();
+	else
+		return false;
+}
+
 namespace cybervision{
 	Sculptor::Sculptor(const QList<QVector3D>& points){
-		if(!points.empty())
-			createSurface(points);
+		if(!points.empty()){
+			createSurface2(points);
+			//createSurface(points);
+		}
 	}
 
 	Surface Sculptor::getSurface()const{ return surface; }
 
-	void Sculptor::createSurface(const QList<QVector3D> &points){
+
+	QList<QVector3D> Sculptor::filterPoints(const QList<QVector3D>& points)const{
+		QList<QVector3D> filteredPoints;
+		QMap<QPointF,float> pointsMap;
+		for(QList<QVector3D>::const_iterator it= points.begin();it!=points.end();it++){
+			QPointF point(it->x(),it->y());
+			pointsMap.insertMulti(point,it->z());
+		}
+		QPointF last_point= pointsMap.begin().key();
+		float sum=0;
+		int count=0;
+		for(QMap<QPointF,float>::const_iterator it=pointsMap.begin();it!=pointsMap.end();it++){
+			if(it.key()==last_point){
+				sum+= it.value();
+				count++;
+			}else{
+				if(count>0)
+				filteredPoints.push_back(QVector3D(it.key().x(),it.key().y(),sum/(float)count));
+				sum= 0;
+				count= 0;
+				last_point= it.key();
+			}
+		}
+		return filteredPoints;
+	}
+
+	void Sculptor::createSurface(const QList<QVector3D> &unfilteredPoints){
+		QList<QVector3D> points=filterPoints(unfilteredPoints);
+
 		//Get data from the point set
 		QVector3D centroid(0,0,0);
 		QVector3D min( std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity());
@@ -167,5 +206,99 @@ namespace cybervision{
 	QVector3D  Sculptor::calcNormal(const QVector3D& a, const QVector3D& b)const{
 		QVector3D dotProduct=QVector3D::crossProduct(a,b);
 		return dotProduct/dotProduct.length();
+	}
+
+
+	void Sculptor::createSurface2(const QList<QVector3D> &unfilteredPoints){
+		QList<QVector3D> points=filterPoints(unfilteredPoints);
+
+		//Get data from the point set
+		QVector3D centroid(0,0,0);
+		QVector3D min( std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity());
+		QVector3D max(-std::numeric_limits<float>::infinity(),-std::numeric_limits<float>::infinity(),-std::numeric_limits<float>::infinity());
+		for(QList<QVector3D>::const_iterator it= points.begin();it!=points.end();it++){
+			min.setX(qMin(min.x(),it->x()));
+			min.setY(qMin(min.y(),it->y()));
+			min.setZ(qMin(min.z(),it->z()));
+			max.setX(qMax(max.x(),it->x()));
+			max.setY(qMax(max.y(),it->y()));
+			max.setZ(qMax(max.z(),it->z()));
+
+			centroid.setX(centroid.x()+it->x());
+			centroid.setY(centroid.x()+it->y());
+			centroid.setZ(centroid.x()+it->z());
+		}
+
+		boundingBox.clear();
+		boundingBox<<QPointF(min.x(),min.y())<<QPointF(max.x(),min.y())<<QPointF(max.x(),max.y())<<QPointF(min.x(),max.y());
+		float scale_z=(max.z()-min.z())/10;
+		float step_x= (max.x()-min.x())/(qreal)Options::surfaceSteps;
+		float step_y= (max.y()-min.y())/(qreal)Options::surfaceSteps;
+		float aspectRatio= (max.x()-min.x())/(max.y()-min.y());
+
+
+
+		for(QList<QVector3D>::const_iterator point= points.begin();point!=points.end();point++){
+			QPolygonF cell= voronoiCell(points,*point);
+
+			//Convert Voronoi cell into a set of triangles
+			for(int i=1;i<cell.size()-1;i++){
+				Surface::Triangle triangle;
+				float z= (point->z()-min.z())/scale_z;
+				triangle.a= QVector3D(cell.at(0).x()/100,cell.at(0).y()/100,-z);
+				triangle.b= QVector3D(cell.at(i).x()/100,cell.at(i).y()/100,-z);
+				triangle.c= QVector3D(cell.at(i+1).x()/100,cell.at(i+1).y()/100,-z);
+				triangle.normal= calcNormal(triangle.b-triangle.a,triangle.c-triangle.a);
+				triangle.normal.setZ(1);
+				//if(triangle.normal.z()<0)
+				//	triangle.normal.setZ(-triangle.normal.z());
+				surface.triangles.push_back(triangle);
+			}
+		}
+	}
+
+
+	QPolygonF Sculptor::voronoiCell(const QList<QVector3D>& points, const QVector3D& center){
+		QPolygonF cell= boundingBox;
+		//Sutherland-Hodgman for the win!
+		for(QList<QVector3D>::const_iterator point= points.begin();point!=points.end();point++){
+			if(qFuzzyCompare(center,*point))
+				continue;
+
+			//Create separation line
+			QLineF lSeparator(center.x(),center.y(),point->x(),point->y());
+			QPointF separatorCenter((center.x()+point->x())/2,(center.y()+point->y())/2);
+			lSeparator.setAngle(lSeparator.angle()+90);
+			lSeparator.translate((point->x()-center.x())/2,(point->y()-center.y())/2);
+
+			QPolygonF new_cell;
+			//Clip every polygon's edge
+			for(QPolygonF::const_iterator edgeStart= cell.begin();edgeStart!=cell.end();edgeStart++){
+				QPolygonF::const_iterator edgeEnd= edgeStart+1;
+				if(edgeEnd==cell.end())
+					edgeEnd= cell.begin();
+
+				QLineF edge(edgeStart->x(),edgeStart->y(),edgeEnd->x(),edgeEnd->y());
+
+				QPointF intersectionPoint;
+				edge.intersect(lSeparator,&intersectionPoint);
+
+				QLineF lSeparatorStart2EdgeStart(lSeparator.p1(),*edgeStart);
+				QLineF lSeparatorStart2EdgeEnd(lSeparator.p1(),*edgeEnd);
+
+				bool edgeStartInside= lSeparator.angleTo(lSeparatorStart2EdgeStart)<180.0;
+				bool edgeEndInside= lSeparator.angleTo(lSeparatorStart2EdgeEnd)<180.0;
+
+				if(edgeEndInside){
+					if(!edgeStartInside)
+						new_cell<<intersectionPoint;
+					new_cell<<*edgeEnd;
+				}else if(edgeStartInside)
+					new_cell<<intersectionPoint;
+			}
+			cell= new_cell;
+		}
+
+		return cell;
 	}
 }
--- 3dvision/Reconstruction/sculptor.h
+++ 3dvision/Reconstruction/sculptor.h
@@ -3,6 +3,7 @@
 
 #include <QList>
 #include <QVector3D>
+#include <QPolygonF>
 
 namespace cybervision{
 	class Surface;
@@ -12,13 +13,22 @@ namespace cybervision{
 	protected:
 
 		cybervision::Surface surface;
+		QPolygonF boundingBox;//For Voronoi cells, this gets clipped for every cell
+
+		QList<QVector3D> filterPoints(const QList<QVector3D>& points)const;//average points with the same (x,y) values
 
 		//Interpolates points to create surface
 		void createSurface(const QList<QVector3D>& points);
 		//Returns points belonging to a specific coordinate range
 		QList<QVector3D> getPointsInRange(const QList<QVector3D>& points,QVector3D min, QVector3D max, bool ignoreZ=true)const;
-
+		//Returns a normal for a 3D triangle
 		QVector3D calcNormal(const QVector3D& a, const QVector3D& b)const;
+
+
+		//Interpolates points to create surface (using Voronoi cells)
+		void createSurface2(const QList<QVector3D>& points);
+
+		QPolygonF voronoiCell(const QList<QVector3D>& points, const QVector3D& center);
 	public:
 		Sculptor(const QList<QVector3D>& points=QList<QVector3D>());
 
