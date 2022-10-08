d21ed5187b9f98d3f2617ae4df8266da9db59ccb
Dmitry Zolotukhin <zlogic@gmail.com>
2022-10-08 15:40:16 +0200

    Fixed a linking bug with in modern GCC/CMake.

--- cybervision/configuration.h
+++ cybervision/configuration.h
@@ -3,41 +3,41 @@
 
 #include <stddef.h>
 
-const int cybervision_fast_threshold;
-const int cybervision_fast_mode;
-const int cybervision_fast_nonmax;
+extern const int cybervision_fast_threshold;
+extern const int cybervision_fast_mode;
+extern const int cybervision_fast_nonmax;
 
-const float cybervision_correlation_threshold;
-const int cybervision_correlation_kernel_size;
+extern const float cybervision_correlation_threshold;
+extern const int cybervision_correlation_kernel_size;
 
-const int cybervision_keypoint_scale_min_size;
+extern const int cybervision_keypoint_scale_min_size;
 
-const float cybervision_ransac_min_length;
-const size_t cybervision_ransac_k;
-const size_t cybervision_ransac_n_affine;
-const size_t cybervision_ransac_n_perspective;
-const float cybervision_ransac_collinear_epsilon;
-const float cybervision_ransac_t;
-const size_t cybervision_ransac_d;
-const size_t cybervision_ransac_check_interval;
+extern const float cybervision_ransac_min_length;
+extern const size_t cybervision_ransac_k;
+extern const size_t cybervision_ransac_n_affine;
+extern const size_t cybervision_ransac_n_perspective;
+extern const float cybervision_ransac_collinear_epsilon;
+extern const float cybervision_ransac_t;
+extern const size_t cybervision_ransac_d;
+extern const size_t cybervision_ransac_check_interval;
 
-const int cybervision_crosscorrelation_scale_min_size;
-const int cybervision_crosscorrelation_kernel_size;
-const float cybervision_crosscorrelation_threshold;
-const int cybervision_crosscorrelation_corridor_size;
-const int cybervision_crosscorrelation_corridor_segment_length;
-const int cybervision_crosscorrelation_search_area_segment_length;
-const int cybervision_crosscorrelation_neighbor_distance;
-const float cybervision_crosscorrelation_max_slope;
-const int cybervision_crosscorrelation_match_limit;
+extern const int cybervision_crosscorrelation_scale_min_size;
+extern const int cybervision_crosscorrelation_kernel_size;
+extern const float cybervision_crosscorrelation_threshold;
+extern const int cybervision_crosscorrelation_corridor_size;
+extern const int cybervision_crosscorrelation_corridor_segment_length;
+extern const int cybervision_crosscorrelation_search_area_segment_length;
+extern const int cybervision_crosscorrelation_neighbor_distance;
+extern const float cybervision_crosscorrelation_max_slope;
+extern const int cybervision_crosscorrelation_match_limit;
 
 typedef enum 
 { 
     CORRELATION_MODE_CPU = 0,
     CORRELATION_MODE_GPU = 1
 } correlation_mode;
-const correlation_mode cybervision_crosscorrelation_default_mode;
+extern const correlation_mode cybervision_crosscorrelation_default_mode;
 
-const float cybervision_interpolation_epsilon;
+extern const float cybervision_interpolation_epsilon;
 
 #endif
