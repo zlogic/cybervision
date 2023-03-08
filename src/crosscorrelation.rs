f969bca3da39aeb79a223952a9cd69cd50efc381
Dmitry Zolotukhin <zlogic@gmail.com>
2023-03-08 04:15:04 +0100

    Reduce corridor size (1024 causes device to be lost).

--- src/crosscorrelation.rs
+++ src/crosscorrelation.rs
@@ -14,7 +14,7 @@ const MIN_STDEV_AFFINE: f32 = 1.0;
 const MIN_STDEV_PERSPECTIVE: f32 = 1.0;
 const CORRIDOR_SIZE: usize = 20;
 // Decrease when using a low-powered GPU
-const CORRIDOR_SEGMENT_LENGTH: usize = 1024;
+const CORRIDOR_SEGMENT_LENGTH: usize = 512;
 const SEARCH_AREA_SEGMENT_LENGTH: usize = 1024;
 const NEIGHBOR_DISTANCE: usize = 10;
 const CORRIDOR_EXTEND_RANGE: f64 = 1.0;
